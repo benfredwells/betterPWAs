f7bc85eda0ee6f8fb6423980924c45b26c134302
benfred <benfred@mjaw.com>
2024-10-25 18:43:25 +1100

    Actually injects a manifest

--- content-script.js
+++ content-script.js
@@ -1,5 +1,22 @@
-var link = document.createElement('link');
-link.href="/manifest.json"
-document.getElementsByTagName('head')[0].appendChild(link);
+const manifest = {
+  name: "Sydney Morning Herald",
+  short_name: "SMH",
+  display: "minimal-ui",
+  theme_color: "#0A1633",
+  background_color: "#ffffff",
+  icons: [
+    {
+      src: "https://www.smh.com.au/apple-touch-icons/smh.png",
+      sizes: "180x180",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+  ],
+  start_url: window.location.href,
+};
 
-console.log("Added manifest link");
+const link = document.createElement("link");
+link.rel = "manifest";
+link.href = `data:application/json;base64,${btoa(JSON.stringify(manifest))}`;
+
+document.head.appendChild(link);
