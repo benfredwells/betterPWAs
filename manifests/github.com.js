7a42326fd2e1d70e135d7359cd8ea2569e3a5edd
benfred <benfred@mjaw.com>
2024-10-28 20:35:46 +1100

    Tweak github settings

--- manifests/github.com.js
+++ manifests/github.com.js
@@ -6,55 +6,56 @@ const manifest = {
   display_override: ["tabbed"],
   tabstrip: {
     home_tab: {
-      // This should be configurable
-      pathname: "pulls",
+      scope_patterns:
+        // This should be configurable
+        [{ pathname: "pulls" }],
     },
   },
   icons: [
     {
       sizes: "114x114",
       src: "https://github.githubassets.com/assets/apple-touch-icon-114x114-09ce42d3ca4b.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "120x120",
       src: "https://github.githubassets.com/assets/apple-touch-icon-120x120-92bd46d04241.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "144x144",
       src: "https://github.githubassets.com/assets/apple-touch-icon-144x144-b882e354c005.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "152x152",
       src: "https://github.githubassets.com/assets/apple-touch-icon-152x152-5f777cdc30ae.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "180x180",
       src: "https://github.githubassets.com/assets/apple-touch-icon-180x180-a80b8e11abe2.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "57x57",
       src: "https://github.githubassets.com/assets/apple-touch-icon-57x57-22f09f5b3a64.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "60x60",
       src: "https://github.githubassets.com/assets/apple-touch-icon-60x60-19037ac897bf.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "72x72",
       src: "https://github.githubassets.com/assets/apple-touch-icon-72x72-e090c8a282d0.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       sizes: "76x76",
       src: "https://github.githubassets.com/assets/apple-touch-icon-76x76-a4523d80afb4.png",
-      purpose: "any maskable",
+      purpose: "any",
     },
     {
       src: "https://github.githubassets.com/assets/app-icon-192-bcc967ab9829.png",
